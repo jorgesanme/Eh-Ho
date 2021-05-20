cf30b948cf0a8fd971086751bf5c10357187d665
jorgesanme <jorgesanme@gmail.com>
2021-05-20 10:36:46 +0100

    cambios en las vistas del logIn, register y recycleView

--- app/src/main/java/io/keepcoding/eh_ho/topics/TopicsActivity.kt
+++ app/src/main/java/io/keepcoding/eh_ho/topics/TopicsActivity.kt
@@ -1,28 +1,40 @@
 package io.keepcoding.eh_ho.topics
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.os.Handler
 import android.widget.LinearLayout
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import io.keepcoding.eh_ho.R
 import io.keepcoding.eh_ho.databinding.ActivityTopicsBinding
 import io.keepcoding.eh_ho.di.DIProvider
+import kotlinx.coroutines.Runnable
 
 class TopicsActivity : AppCompatActivity() {
 
-    private val binding: ActivityTopicsBinding by lazy { ActivityTopicsBinding.inflate(layoutInflater) }
+    private val binding: ActivityTopicsBinding by lazy {
+        ActivityTopicsBinding.inflate(
+            layoutInflater
+        )
+    }
     private val topicsAdapter = TopicsAdapter()
     private val vm: TopicsViewModel by viewModels { DIProvider.topicsViewModelProviderFactory }
 
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        iniRefreshListener()
         binding.topics.apply {
             adapter = topicsAdapter
-//            addItemDecoration(DividerItemDecoration(this@TopicsActivity, LinearLayout.VERTICAL))
         }
         vm.state.observe(this) {
             when (it) {
@@ -38,12 +50,30 @@ class TopicsActivity : AppCompatActivity() {
         vm.loadTopics()
     }
 
+    private fun iniRefreshListener(){
+        val swipeContainer = binding.refresh
+        swipeContainer.setOnRefreshListener(SwipeRefreshLayout.OnRefreshListener {
+            // TODO: 19/5/21  se debe borrar el contenido de las vista y volver a cargarlo
+            // vm.loadTopics()
+            val handler = Handler()
+            Handler().postDelayed(Runnable{
+                if(swipeContainer.isRefreshing()){
+                    swipeContainer.isRefreshing = false
+                }
+            }, 2000)
+        })
+    }
+
     private fun renderEmptyState() {
         // Render empty state
     }
 
     private fun renderLoading(loadingState: TopicsViewModel.State.LoadingTopics) {
-        (loadingState as? TopicsViewModel.State.LoadingTopics.LoadingWithTopics)?.let { topicsAdapter.submitList(it.topics) }
+        (loadingState as? TopicsViewModel.State.LoadingTopics.LoadingWithTopics)?.let {
+            topicsAdapter.submitList(
+                it.topics
+            )
+        }
     }
 
     companion object {
--- app/src/main/java/io/keepcoding/eh_ho/topics/TopicsAdapter.kt
+++ app/src/main/java/io/keepcoding/eh_ho/topics/TopicsAdapter.kt
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
+import io.keepcoding.eh_ho.R
 import io.keepcoding.eh_ho.databinding.ViewTopicBinding
 import io.keepcoding.eh_ho.extensions.inflater
 import io.keepcoding.eh_ho.model.Topic
@@ -17,6 +18,7 @@ class TopicsAdapter(diffUtilItemCallback: DiffUtil.ItemCallback<Topic> = DIFF) :
 
     override fun onBindViewHolder(holder: TopicViewHolder, position: Int) =
         holder.bind(getItem(position))
+
 
     companion object {
         val DIFF = object : DiffUtil.ItemCallback<Topic>() {
@@ -39,8 +41,11 @@ class TopicsAdapter(diffUtilItemCallback: DiffUtil.ItemCallback<Topic> = DIFF) :
             binding.topicCount.text = topic.postsCount.toString()
             binding.replayCount.text = topic.replyCount.toString()
             binding.lastPostDate.text = topic.lastPostedAt
+
             Picasso.get()
-                .load(topic.imageURL)
+                .load("https://unsplash.com/photos/YNliXm_hMn8")
+                .placeholder(R.drawable.user_icon)
+                .error(R.drawable.error)
                 .into(binding.avatarImage)
         }
     }
